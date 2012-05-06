8721d42df15cb884335fe8dd3824e09b1e60877f
Ryan Michael <kerinin@gmail.com>
2012-05-05 23:08:39 -0500

    fixing tests

--- spec/daggregator_spec.rb
+++ spec/daggregator_spec.rb
@@ -24,74 +24,43 @@ describe Daggregator do
 
     describe "get_node" do
       it "calls connection" do
-        l = Daggregator.config.get_node_cb
-        @c.should_receive(:get).with('/node/foo', l)
+        @c.should_receive(:get).with('/nodes/foo.json')
         subject.get_node('foo')      
-      end
-      
-      it "takes a callback" do
-        l = lambda {|cb| puts cb}
-        @c.should_receive(:get).with('/node/foo', l)
-        subject.get_node('foo', &l)
       end
     end
 
     describe "get_aggregates" do
       it "calls connection" do
-        l = Daggregator.config.get_aggregates_cb
-        @c.should_receive(:get).with('/node/foo/sum/bar', l)
+        @c.should_receive(:get).with('/nodes/foo/sum/bar.json')
         subject.get_aggregates('foo', 'sum', 'bar')      
-      end
-      
-      it "takes a callback" do
-        l = lambda {|cb| puts cb}
-        @c.should_receive(:get).with('/node/foo/sum/bar', l)
-        subject.get_aggregates('foo', 'sum', 'bar', &l)      
       end
 
       it "takes an array of attributes" do
-        l = Daggregator.config.get_aggregates_cb
-        @c.should_receive(:get).with('/node/foo/sum/bar+baz', l)
+        @c.should_receive(:get).with('/nodes/foo/sum/bar+baz.json')
         subject.get_aggregates('foo', 'sum', ['bar', 'baz'])      
       end
 
       it "accepts symbols" do
-        l = Daggregator.config.get_aggregates_cb
-        @c.should_receive(:get).with('/node/foo/sum/bar+baz', l)
+        @c.should_receive(:get).with('/nodes/foo/sum/bar+baz.json')
         subject.get_aggregates(:foo, :sum, [:bar, :baz])
       end
     end
 
     describe "put_node" do
       it "calls connection" do
-        l = Daggregator.config.put_node_cb
-        @c.should_receive(:put).with('/node/foo', {:bar => 1, :baz => 2}, l)
+        @c.should_receive(:put).with('/nodes/foo.json', {:bar => 1, :baz => 2})
         subject.put_node('foo', {:bar => 1, :baz => 2})      
-      end
-      
-      it "takes a callback" do
-        l = lambda {|cb| puts cb}
-        @c.should_receive(:put).with('/node/foo', {:bar => 1, :baz => 2}, l)
-        subject.put_node('foo', {:bar => 1, :baz => 2}, &l)
       end
     end
 
     describe "put_flow" do
       it "calls connection" do
-        l = Daggregator.config.put_flow_cb
-        @c.should_receive(:put).with('/node/source/flow_to/target', l)
+        @c.should_receive(:put).with('/nodes/source/flow_to/target.json')
         subject.put_flow('source', 'target')      
-      end
-      
-      it "takes a callback" do
-        l = lambda {|cb| puts cb}
-        @c.should_receive(:put).with('/node/source/flow_to/target', l)
-        subject.put_flow('source', 'target', &l)
       end
 
       it "accepts an arry of targets" do
-        l = Daggregator.config.put_flow_cb
-        @c.should_receive(:put).with('/node/source/flow_to/target1+target2', l)
+        @c.should_receive(:put).with('/nodes/source/flow_to/target1+target2.json')
         subject.put_flow('source', ['target1', 'target2'])
       end
     end
